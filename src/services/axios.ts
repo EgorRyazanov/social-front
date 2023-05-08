be0399745e0ec28efab62c33053a695c6cf889f6
AricsyGIT <aricsybsn@gmail.com>
2023-05-09 01:29:02 +0500

    fix: rul

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://158.160.48.220";
+export const API_URL = "https://stellar.burger.students.nomoredomains.monster";
 
 const $api = axios.create({
     baseURL: API_URL,
